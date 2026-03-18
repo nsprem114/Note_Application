0c3654573f7f556590b1b803f64947c8d88a952d
Prem Kumar Pathak <nsprem114@gmail.com>
2026-03-18 17:20:22 +0530

    prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import path from "path";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 const app = express();
@@ -19,13 +19,17 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json());
 app.use(rateLimiter);
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
